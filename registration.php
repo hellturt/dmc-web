34202832c2874ce05612c1d4c907c6b7411bc78c
Faiz Mohd <faiz@massiveinfinity.com>
2023-11-25 20:44:37 +0800

    replace register folder with php file

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Second Hope";
+$pageDescription = "This is the Second Hope page of our website.";
+include 'component/header.php';
+?>
+
+<main id="primary" class="site-main main-second-hope">
+
+</main>
+
+<?php include 'component/footer.php'; ?>
