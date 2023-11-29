5fa2cca1c2c3e5909ed81e9d928b8ee33819757a
faezah19 <148043750+faezah19@users.noreply.github.com>
2023-11-29 09:35:33 +0800

    Update index.php - hide spin box

--- registration/index.php
+++ registration/index.php
@@ -52,6 +52,14 @@ $dotenv->load();
         .opacity-100 {
             opacity: 100;
         }
+
+        input[type=number]::-webkit-inner-spin-button, 
+        input[type=number]::-webkit-outer-spin-button { 
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+                 margin: 0; 
+        }
     </style>
 
 
